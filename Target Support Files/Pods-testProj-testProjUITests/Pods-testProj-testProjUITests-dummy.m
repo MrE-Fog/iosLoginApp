2b85fa027d21f6027e82ce41f590c05e30de8d47
Akash <akashdeepsaha03@gmail.com>
2023-01-17 14:24:28 -0800

    Initial Commit

---
+++ Target Support Files/Pods-testProj-testProjUITests/Pods-testProj-testProjUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testProj_testProjUITests : NSObject
+@end
+@implementation PodsDummy_Pods_testProj_testProjUITests
+@end
---
+++ Target Support Files/Pods-testProjTests/Pods-testProjTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_testProjTests : NSObject
+@end
+@implementation PodsDummy_Pods_testProjTests
+@end
